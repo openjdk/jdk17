3ef5b96a4db5c8be006a6cf6e7b345a643a4fe67
Aleksey Shipilev <shade@redhat.com>
2021-06-30 12:25:40 +0200

    Just filter signature interfaces

--- src/hotspot/share/opto/callGenerator.cpp
+++ src/hotspot/share/opto/callGenerator.cpp
@@ -1148,13 +1148,14 @@ CallGenerator* CallGenerator::for_method_handle_inline(JVMState* jvms, ciMethod*
         // Cast receiver to its type.
         if (!target->is_static()) {
           Node* arg = kit.argument(0);
+          ciKlass* sig_klass = signature->accessing_klass();
           const TypeOopPtr* arg_type = arg->bottom_type()->isa_oopptr();
-          const Type*       sig_type = TypeOopPtr::make_from_klass(signature->accessing_klass());
+          const Type*       sig_type = TypeOopPtr::make_from_klass(sig_klass);
           if (arg_type != NULL && !arg_type->higher_equal(sig_type)) {
-            // Keep the speculative part, but disallow it to empty the type
-            const Type* recv_type = arg_type->join_speculative(sig_type);
-            const Type* cast_type = recv_type->empty() ? sig_type : recv_type;
-            Node* cast_obj = gvn.transform(new CheckCastPPNode(kit.control(), arg, cast_type));
+            // Keep the speculative part, but do not join with interfaces
+            const Type* recv_type = sig_klass->is_interface() ?
+                    sig_type : arg_type->join_speculative(sig_type);
+            Node* cast_obj = gvn.transform(new CheckCastPPNode(kit.control(), arg, recv_type));
             kit.set_argument(0, cast_obj);
           }
         }
@@ -1163,13 +1164,14 @@ CallGenerator* CallGenerator::for_method_handle_inline(JVMState* jvms, ciMethod*
           ciType* t = signature->type_at(i);
           if (t->is_klass()) {
             Node* arg = kit.argument(receiver_skip + j);
+            ciKlass* sig_klass = t->as_klass();
             const TypeOopPtr* arg_type = arg->bottom_type()->isa_oopptr();
-            const Type*       sig_type = TypeOopPtr::make_from_klass(t->as_klass());
+            const Type*       sig_type = TypeOopPtr::make_from_klass(sig_klass);
             if (arg_type != NULL && !arg_type->higher_equal(sig_type)) {
-              // Keep the speculative part, but disallow it to empty the type
-              const Type* narrowed_arg_type = arg_type->join_speculative(sig_type);
-              const Type* cast_type = narrowed_arg_type->empty() ? sig_type : narrowed_arg_type;
-              Node* cast_obj = gvn.transform(new CheckCastPPNode(kit.control(), arg, cast_type));
+              // Keep the speculative part, but do not join with interfaces
+              const Type* narrowed_arg_type = sig_klass->is_interface() ?
+                      sig_type : arg_type->join_speculative(sig_type);
+              Node* cast_obj = gvn.transform(new CheckCastPPNode(kit.control(), arg, narrowed_arg_type));
               kit.set_argument(receiver_skip + j, cast_obj);
             }
           }
