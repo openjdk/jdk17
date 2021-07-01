3ed97631b2e15580398ada120fec609607bd7d31
Nils Eliasson <nils.eliasson@oracle.com>
2021-07-01 23:03:01 +0200

    Fix assert

--- src/hotspot/share/opto/macro.cpp
+++ src/hotspot/share/opto/macro.cpp
@@ -963,11 +963,12 @@ void PhaseMacroExpand::process_users_of_allocation(CallNode *alloc) {
         assert(init->outcnt() <= 2, "only a control and memory projection expected");
         Node *ctrl_proj = init->proj_out_or_null(TypeFunc::Control);
         if (ctrl_proj != NULL) {
-#ifdef ASSERT
-          Node* tmp = init->in(TypeFunc::Control);
-          assert(tmp == _callprojs.fallthrough_catchproj, "allocation control projection");
-#endif
           _igvn.replace_node(ctrl_proj, init->in(TypeFunc::Control));
+#ifdef ASSERT
+          // If the InitializeNode has no memory out, it will die, and tmp will become NULL
+          Node* tmp = init->in(TypeFunc::Control);
+          assert(tmp == NULL || tmp == _callprojs.fallthrough_catchproj, "allocation control projection");
+#endif
         }
         Node *mem_proj = init->proj_out_or_null(TypeFunc::Memory);
         if (mem_proj != NULL) {
