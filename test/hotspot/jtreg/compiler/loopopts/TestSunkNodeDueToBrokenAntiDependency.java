50e09ec4d4bb8793055e93c0b084e5537be6ea15
rwestrel <rwestrel@redhat.com>
2021-07-06 08:56:13 +0200

    test cleanup

--- test/hotspot/jtreg/compiler/loopopts/TestSunkNodeDueToBrokenAntiDependency.java
+++ test/hotspot/jtreg/compiler/loopopts/TestSunkNodeDueToBrokenAntiDependency.java
@@ -37,7 +37,7 @@ public class TestSunkNodeDueToBrokenAntiDependency {
     public static volatile long instanceCount=-154L;
     public volatile int iArrFld[]=new int[N];
 
-    public void mainTestSunkNodeDueToBrokenAntiDependency(String[] strArr1) {
+    public void mainTest() {
 
         int i8=8, i9=-3, i10=-199, i11=13, i12=8, i13=2;
         long lArr1[]=new long[N];
@@ -46,19 +46,19 @@ public class TestSunkNodeDueToBrokenAntiDependency {
             for (i8 = 1; i8 < 63; ++i8) {
                 i10 = 1;
                 while (++i10 < 2) {
-                    i7 += (int)TestSunkNodeDueToBrokenAntiDependency.instanceCount;
+                    i7 += (int)instanceCount;
                     lArr1[i10 + 1] -= 3;
                 }
                 i11 = 2;
                 do {
                     byte by2=-104;
-                    by2 = (byte)TestSunkNodeDueToBrokenAntiDependency.instanceCount;
+                    by2 = (byte)instanceCount;
                 } while (--i11 > 0);
                 i9 <<= 6;
                 for (i12 = i8; 2 > i12; i12++) {
                     switch (((i11 >>> 1) % 1) + 66) {
                     case 66:
-                        TestSunkNodeDueToBrokenAntiDependency.instanceCount -= i13;
+                        instanceCount -= i13;
                         break;
                     }
 
@@ -69,7 +69,7 @@ public class TestSunkNodeDueToBrokenAntiDependency {
     public static void main(String[] strArr) {
         TestSunkNodeDueToBrokenAntiDependency _instance = new TestSunkNodeDueToBrokenAntiDependency();
         for (int i = 0; i < 10; i++ ) {
-            _instance.mainTestSunkNodeDueToBrokenAntiDependency(strArr);
+            _instance.mainTest();
         }
     }
 }
