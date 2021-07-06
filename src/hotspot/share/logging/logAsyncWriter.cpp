3418758e4d9895be57b8a1dd4e91d5aa0283e5cc
Xin Liu <xxinliu@amazon.com>
2021-07-06 19:00:28 +0000

    Revert "8269865: Async UL needs to handle ERANGE on exceeding SEM_VALUE_MAX"
    This reverts commit 269a1580990fa955c1f9d37ce1d04a6349959992.

--- src/hotspot/share/logging/logAsyncWriter.cpp
+++ src/hotspot/share/logging/logAsyncWriter.cpp
@@ -109,7 +109,7 @@ class AsyncLogMapIterator {
   }
 };
 
-int AsyncLogWriter::write() {
+void AsyncLogWriter::write() {
   // Use kind of copy-and-swap idiom here.
   // Empty 'logs' swaps the content with _buffer.
   // Along with logs destruction, all processed messages are deleted.
@@ -117,13 +117,11 @@ int AsyncLogWriter::write() {
   // The operation 'pop_all()' is done in O(1). All I/O jobs are then performed without
   // lock protection. This guarantees I/O jobs don't block logsites.
   AsyncLogBuffer logs;
-  int dequeued  = 0;
 
   { // critical region
     AsyncLogLocker locker;
 
     _buffer.pop_all(&logs);
-    dequeued = logs.size();
     // append meta-messages of dropped counters
     AsyncLogMapIterator dropped_counters_iter(logs);
     _stats.iterate(&dropped_counters_iter);
@@ -150,7 +148,6 @@ int AsyncLogWriter::write() {
     assert(req == 1, "AsyncLogWriter::flush() is NOT MT-safe!");
     _flush_sem.signal(req);
   }
-  return dequeued;
 }
 
 void AsyncLogWriter::run() {
@@ -158,11 +155,7 @@ void AsyncLogWriter::run() {
     // The value of a semphore cannot be negative. Therefore, the current thread falls asleep
     // when its value is zero. It will be waken up when new messages are enqueued.
     _sem.wait();
-
-    int n = write();
-    for (int i = n - 1; i > 0; --i) {
-      _sem.wait();
-    }
+    write();
   }
 }
 
--- src/hotspot/share/logging/logAsyncWriter.hpp
+++ src/hotspot/share/logging/logAsyncWriter.hpp
@@ -152,7 +152,7 @@ class AsyncLogWriter : public NonJavaThread {
 
   AsyncLogWriter();
   void enqueue_locked(const AsyncLogMessage& msg);
-  int write();
+  void write();
   void run() override;
   void pre_run() override {
     NonJavaThread::pre_run();
